3251190bb76224236bafe4ab9cae033cb78fc09b
unknown <2013burl01@gmail.com>
2025-04-18 03:55:26 +0200

    Update project

---
+++ font.py
@@ -0,0 +1,31 @@
+from docx import Document
+from docx.shared import Pt, RGBColor
+import os
+#import psutil
+
+doc = Document("reduta.docx")
+
+# bold every next max 3 letters of the word
+def first_3(word, num, paragraph):
+    for i, litera in enumerate(word):
+        run = paragraph.add_run(litera)
+        if i < num:
+            run.bold = True
+    paragraph.add_run(" ")
+
+new_doc = Document()
+
+# checking lines -> words split in lines
+for para in doc.paragraphs:
+    p = new_doc.add_paragraph() 
+    words = para.text.split()
+    for word in words:
+        first_3(word, 3, p)
+
+new_doc.save("reduta_enhanced.docx")
+
+# opening + closing
+os.startfile("reduta_enhanced.docx")
+k = input()
+os.system("taskkill /f /im WINWORD.EXE")
+print(k)
